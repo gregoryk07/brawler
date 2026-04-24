13b10db77d420c0062aac39821679db5d6277184
gregoryk07 <gregory.k07.mail@gmail.com>
2026-04-24 23:18:20 +0200

    created input.js

---
+++ input.js
@@ -0,0 +1,12 @@
+var input = {
+    left: false,
+    right: false,
+    jump: false,
+    crouch: false,
+    attack: false,
+    menu: false
+}
+
+function runInput(){
+    console.log("INPUT");
+}
