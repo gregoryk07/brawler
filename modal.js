6948b0c46a31aa57a1b13728cf2090cc1e1baf69
Grzegorz Krupa <grzegorz.krupa2016@hotmail.com>
2026-05-25 18:30:22 +0200

    added modal.js

---
+++ modal.js
@@ -0,0 +1,17 @@
+abletohidemodal = true;
+
+function showmodal(text, showbutton = false, canhidemodal = true){
+    ismodalshown = true;
+    abletohidemodal = canhidemodal;
+    document.getElementById("modal").innerHTML = "<h2>" + text + "</h2>" + (showbutton ? "<button class='modalbtn' onclick='hidemodal()'>OK</button>" : "");
+    document.getElementById("modal").style.display = "block";
+}
+function hidemodal(force = false){
+    if(abletohidemodal || force)
+    {
+        ismodalshown = false;
+        document.getElementById("modal").style.display = "none";
+    }
+}
+
+hidemodal(true);
