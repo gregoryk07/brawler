c92123ddae47a2a203fd1b31d895d32a4da1f54b
gregoryk07 <gregory.k07.mail@gmail.com>
2026-06-06 14:55:49 +0200

    fixed control boolean to know when modal is visible

--- modal.js
+++ modal.js
@@ -1,9 +1,10 @@
 abletohidemodal = true;
+ismodalshown = false;
 
 function showmodal(text, showbutton = false, canhidemodal = true){
     ismodalshown = true;
     abletohidemodal = canhidemodal;
-    document.getElementById("modal").innerHTML = "<h2>" + text + "</h2>" + (showbutton ? "<button class='modalbtn' onclick='hidemodal()'>OK</button>" : "");
+    document.getElementById("modal").innerHTML = "<h2>" + text.split("\n").join("<br>") + "</h2>" + (showbutton ? "<button class='modalbtn' onclick='hidemodal()'>OK</button>" : "");
     document.getElementById("modal").style.display = "block";
 }
 function hidemodal(force = false){
